89e00de041c3e174986021e70e5d391bdb6b5be6
craigdallimore <decoy9697@gmail.com>
2023-02-15 18:10:51 +1300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "See saw scraper";
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs-16_x
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+          shellHook = ''
+            alias gst='git status'
+            alias gd='git diff'
+            alias gc='git commit'
+            :q() {
+              exit
+            }
+          '';
+        };
+      });
+  }
+}
