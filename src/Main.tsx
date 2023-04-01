864fe32cf5d22702c0166cdb686931c55304ef14
craigdallimore <decoy9697@gmail.com>
2023-04-02 09:27:31 +1200

    Remove comment

--- src/Main.tsx
+++ src/Main.tsx
@@ -62,13 +62,6 @@ type Message = {
   content: string
 };
 
-/*
-{"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-*/
-
 export default function Main(props: Props) {
   const [prompt, setPrompt] = React.useState<string>('');
   const [isLoading, setIsLoading] = React.useState(false);
